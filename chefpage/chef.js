1a1b5f4a32a797b3387db75bce72dbc83bc81ffa
Marian George <50915367+georgeMarian1999@users.noreply.github.com>
2021-04-29 17:30:40 +0300

    Jquery slider

--- chefpage/chef.js
+++ chefpage/chef.js
@@ -4,8 +4,6 @@ var index=-1;
 var repeated=false
 function changeImage(){
   var list=document.getElementsByClassName("sliderimage");
-  console.log(index);
-  console.log(list.length);
   if(index<list.length){
     for(var i=0;i<list.length;i++){
       list[i].style.display="none";
@@ -55,21 +53,109 @@ function changeSeconds(){
 }
 document.addEventListener("DOMContentLoaded",function start(){
   changeImage(defaultms);
-  
 });
+function compare(index) {
+  return function(a, b) {
+      var A = $(a).children('td').eq(index).text(), B = $(b).children('td').eq(index).text();
+      return $.isNumeric(A) && $.isNumeric(B) ? A - B : A.toString().localeCompare(B)
+  }
+}
+var ascendent=true;
 $(function(){
   $('th').click(function(){
     var table = $(this).parents('table').eq(0)
     var rows = table.find('tr:gt(0)').toArray().sort(compare($(this).index()))
-    this.asc = !this.asc
-    if (!this.asc){rows = rows.reverse()}
+    ascendent = !ascendent;
+    if (!ascendent){rows = rows.reverse()}
     for (var i = 0; i < rows.length; i++){table.append(rows[i])}
   })
 })
-function compare(index) {
-  return function(a, b) {
-      var valA = getCellValue(a, index), valB = getCellValue(b, index)
-      return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+var awardTime;
+var direction=true;
+var ms=3000;
+function changeAwardSlides(){
+  console.log(direction);
+  if(direction==true){
+    if($('.active').next().length){
+      $slide = $('.active').width();
+    $('.containerSlide').animate({left: '-=' + $slide});
+    $('.active').next().removeClass('next');
+    $('.active').prev().removeClass('prev');
+    $('.active').next().next().addClass('next');
+    
+    $('.active').addClass('prev');
+    $('.active').next().addClass('active');
+    $('.active.prev').removeClass('active');
+    }
+    else {
+      direction=false;
+    }
   }
+  else {
+    if($('.active').prev().length){
+      $slide = $('.active').width();
+      $('.containerSlide').animate({left: '+=' + $slide});
+      $('.active').next().removeClass('next');
+      $('.active').prev().removeClass('prev');
+      $('.active').prev().prev().addClass('prev');
+      
+      $('.active').addClass('next');
+      $('.active').prev().addClass('active');
+      $('.active.next').removeClass('active');
+    }
+    else direction=true;
+  }
+  awardTime=setTimeout(changeAwardSlides,ms);
 }
-function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
+function playJQ(){
+  awardTime=setTimeout(changeAwardSlides,ms);
+  $('#playpause').text("Pause");
+}
+function pauseJQ(){
+  clearTimeout(awardTime);
+  $('#playpause').text("Play");
+}
+jQuery(document).ready(function ($) {
+  $('.slide:nth-child(' +2+ ')').addClass('active');
+  $('.slide:nth-child(' + 2 + ')').prev().addClass('prev');
+  $('.slide:nth-child(' + 2+ ')').next().addClass('next');
+
+  $('#slideprev').click(function(){
+    pauseJQ();
+    $slide = $('.active').width();
+    $('.containerSlide').animate({left: '+=' + $slide});
+    $('.active').next().removeClass('next');
+    $('.active').prev().removeClass('prev');
+    $('.active').prev().prev().addClass('prev');
+    direction=false;
+    $('.active').addClass('next');
+    $('.active').prev().addClass('active');
+    $('.active.next').removeClass('active');
+  
+  })
+  $('#slidenext').click(function(){
+    pauseJQ();
+    $slide = $('.active').width();
+    $('.containerSlide').animate({left: '-=' + $slide});
+    $('.active').next().removeClass('next');
+    $('.active').prev().removeClass('prev');
+    $('.active').next().next().addClass('next');
+    direction=true;
+    $('.active').addClass('prev');
+    $('.active').next().addClass('active');
+    $('.active.prev').removeClass('active');
+  })
+  $('#playpause').click(function(){
+    if($('#playpause').text()=="Pause"){
+      pauseJQ();
+    }
+    else playJQ();
+  })
+  $('#intervalAwards').change(function(){
+    ms=$('#intervalAwards').val()*1000;
+    clearTimeout(awardTime);
+    changeAwardSlides();
+  })
+  changeAwardSlides();
+  
+});
